f55d5f7b5a98df900f76ef54bfb6573d6459791b
Kevin Green <greenk12@tamu.edu>
2023-11-07 19:10:13 -0700

    Wrote documentation for dbeta_post

--- R/one_parameter.R
+++ R/one_parameter.R
@@ -1,3 +1,19 @@
+#' Sample Posterior Beta PDF
+#'
+#' `dbeta_post()` will sample the posterior beta probability distribution function
+#' in a beta-binomial model.
+#'
+#' @param x vector of quantiles
+#' @param successes number of success in the binomial experiment
+#' @param sample_size number of trials in the binomial experiment
+#' @param ... Placeholder for future capabilities. Currently ignored.
+#' @param alpha_prior The prior for the alpha parameter
+#' @param beta_prior The prior for the beta parameter
+#'
+#' @returns A vector of probabilities the same length as x
+#' @export
+#'
+#' @examples
 dbeta_post <- function(
     x, successes, sample_size, ..., alpha_prior = 1/2, beta_prior = 1/2
     ) {
