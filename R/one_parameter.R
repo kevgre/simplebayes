57b1444c64a95def21c3e1583e6c2e66d09600ed
Kevin Green <greenk12@tamu.edu>
2023-11-07 19:17:30 -0700

    Wrote function to sample the posterior beta cdf with test

--- R/one_parameter.R
+++ R/one_parameter.R
@@ -3,20 +3,27 @@
 #' `dbeta_post()` will sample the posterior beta probability distribution function
 #' in a beta-binomial model.
 #'
-#' @param x vector of quantiles
+#' @param quantile vector of quantiles
 #' @param successes number of success in the binomial experiment
 #' @param sample_size number of trials in the binomial experiment
 #' @param ... Placeholder for future capabilities. Currently ignored.
 #' @param alpha_prior The prior for the alpha parameter
 #' @param beta_prior The prior for the beta parameter
 #'
-#' @returns A vector of probabilities the same length as x
+#' @returns A vector of probabilities the same length as quantile
 #' @export
 #'
 #' @examples
 dbeta_post <- function(
-    x, successes, sample_size, ..., alpha_prior = 1/2, beta_prior = 1/2
+    quanitle, successes, sample_size, ..., alpha_prior = 1/2, beta_prior = 1/2
     ) {
   failures <- sample_size - successes
-  stats::dbeta(x = x, shape1 = successes + alpha_prior, shape2 = failures + beta_prior)
+  stats::dbeta(quantile, shape1 = successes + alpha_prior, shape2 = failures + beta_prior)
+}
+
+pbeta_post <- function(
+    quantile, successes, sample_size, ..., alpha_prior = 1/2, beta_prior = 1/2
+    ) {
+  failures <- sample_size - successes
+  stats::pbeta(quantile, successes + alpha_prior, failures + beta_prior, ...)
 }
--- tests/testthat/test-one_parameter.R
+++ tests/testthat/test-one_parameter.R
@@ -1,3 +1,7 @@
 test_that("posterior dbeta works", {
   expect_equal(dbeta_post(1, 10, 100), stats::dbeta(1, 10 + 1/2, 100 + 1/2))
 })
+
+test_that("posterior pbeta works", {
+  expect_equal(pbeta_post(1, 10, 100), stats::pbeta(1, 10 + 1/2, 100 + 1/2))
+})
