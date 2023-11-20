2714651c0318a0bf5092fd7fe4a83a4d80423951
Kevin Green <greenk12@tamu.edu>
2023-11-19 18:59:05 -0700

    Wrote documentation for pbeta_post

--- R/one_parameter.R
+++ R/one_parameter.R
@@ -21,6 +21,21 @@ dbeta_post <- function(
   stats::dbeta(quantile, shape1 = successes + alpha_prior, shape2 = failures + beta_prior)
 }
 
+#' Sample from beta CDF
+#'
+#' Samples from the Cumulative Density Function for the beta distribution.
+#'
+#' @param quantile Vector of quantiles
+#' @param successes The number of successes in the binomial experiment
+#' @param sample_size The sample size for the binomial experiment
+#' @param ... Pass additional arguments to the pbeta function
+#' @param alpha_prior The prior value for alpha
+#' @param beta_prior The prior value for beta
+#'
+#' @returns A vector of probabilities the same length as the quantiles
+#' @export
+#'
+#' @examples
 pbeta_post <- function(
     quantile, successes, sample_size, ..., alpha_prior = 1/2, beta_prior = 1/2
     ) {
