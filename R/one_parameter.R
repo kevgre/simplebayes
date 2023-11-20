d7cd8d61148f96c00c7f613f50711724708dcf29
Kevin Green <greenk12@tamu.edu>
2023-11-20 13:28:07 -0700

    wrote functions for pgamma_post and rgamma_post

--- R/one_parameter.R
+++ R/one_parameter.R
@@ -55,3 +55,13 @@ dgamma_post <- function(quantile, x, sample_size, ..., alpha_prior, beta_prior) 
   x <- sum(x)
   stats::dgamma(quantile, x + alpha_prior, rate = sample_size + beta_prior, ...)
 }
+
+pgamma_post <- function(quantile, x, sample_size, ..., alpha_prior, beta_prior) {
+  x <- sum(x)
+  stats::pgamma(quantile, x + alpha_prior, sample_size + beta_prior, ...)
+}
+
+rgamma_post <- function(n, x, sample_size, ..., alpha_prior, beta_prior) {
+  x <- sum(x)
+  stats::rgamma(n = n, shape = x + alpha_prior, rate = sample_size + beta_prior)
+}
