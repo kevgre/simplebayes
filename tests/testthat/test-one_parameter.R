f3b48df98492b148aeb8634738e2b0ad1d5058bd
Kevin Green <greenk12@tamu.edu>
2023-11-19 19:14:51 -0700

    Updated previous tests to be more accurate and added test for rbeta_post

--- tests/testthat/test-one_parameter.R
+++ tests/testthat/test-one_parameter.R
@@ -1,7 +1,14 @@
 test_that("posterior dbeta works", {
-  expect_equal(dbeta_post(1, 10, 100), stats::dbeta(1, 10 + 1/2, 100 + 1/2))
+  expect_equal(dbeta_post(0.5, 10, 20), stats::dbeta(0.5, 10 + 1/2, 10 + 1/2))
 })
 
 test_that("posterior pbeta works", {
-  expect_equal(pbeta_post(1, 10, 100), stats::pbeta(1, 10 + 1/2, 100 + 1/2))
+  expect_equal(pbeta_post(0.5, 10, 20), stats::pbeta(0.5, 10 + 1/2, 10 + 1/2))
+})
+
+test_that("posterior rbeta works", {
+  withr::with_seed(123, {
+    expect_equal(rbeta_post(1, 10, 20), stats::rbeta(1, 10 + 1/2, 10 + 1/2),
+                 tolerance = 0.1)
+  })
 })
