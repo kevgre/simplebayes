809911673676d875c47e9175e1f6c59d6ad83fc1
Kevin Green <greenk12@tamu.edu>
2023-11-27 22:06:38 -0700

    Corrected quantiles to be in the support

--- tests/testthat/test-beta_priors.R
+++ tests/testthat/test-beta_priors.R
@@ -15,15 +15,15 @@ test_that("beta paramters updated", {
 })
 
 test_that("dbeta_post works", {
-  expect_error(dbeta_post(2, "poisson"))
-  expect_equal(dbeta_post(2, likelihood = "binom", result = 2, sample_size = 10),
-              dbeta(2, 0.5 + 2, 10 - 2 + 0.5))
+  expect_error(dbeta_post(1/2, "poisson"))
+  expect_equal(dbeta_post(1/2, likelihood = "binom", result = 2, sample_size = 10),
+              dbeta(1/2, 0.5 + 2, 10 - 2 + 0.5))
   expect_equal(
-    dbeta_post(2, likelihood = "nbinom", result = 2, sample_size = 10, failure_rate = 0.5),
-               dbeta(2, 0.5 + 10 * 0.5, 2 + 0.5))
-  expect_equal(dbeta_post(2, likelihood = "geometric", result = 2, sample_size = 10),
-               dbeta(2, 0.5 + 10, 2 + 0.5))
-  quants <- c(2, 3, 4, 5, 2, 3)
+    dbeta_post(1/2, likelihood = "nbinom", result = 2, sample_size = 10, failure_rate = 0.5),
+               dbeta(1/2, 0.5 + 10 * 0.5, 2 + 0.5))
+  expect_equal(dbeta_post(1/2, likelihood = "geometric", result = 2, sample_size = 10),
+               dbeta(1/2, 0.5 + 10, 2 + 0.5))
+  quants <- 1/c(2, 3, 4, 5, 2, 3)
   expect_length(
     dbeta_post(quants, "binom", result = 3, sample_size = 10), length(quants)
     )
@@ -34,18 +34,20 @@ test_that("dbeta_post works", {
                  dbeta_post(quants[4], "binom", result = 3, sample_size = 10),
                  dbeta_post(quants[5], "binom", result = 3, sample_size = 10),
                  dbeta_post(quants[6], "binom", result = 3, sample_size = 10)))
+  expect_equal(dbeta_post(0.5, likelihood = "binom", result = 2, sample_size = 10, log = TRUE),
+                          dbeta(0.5, 0.5 + 2, 10 - 2 + 0.5, log = TRUE))
 })
 
 test_that("pbeta_post works", {
-  expect_error(pbeta_post(2, "poisson"))
-  expect_equal(pbeta_post(2, likelihood = "binom", result = 2, sample_size = 10),
-               pbeta(2, 0.5 + 2, 10 - 2 + 0.5))
+  expect_error(pbeta_post(1/2, "poisson"))
+  expect_equal(pbeta_post(1/2, likelihood = "binom", result = 2, sample_size = 10),
+               pbeta(1/2, 0.5 + 2, 10 - 2 + 0.5))
   expect_equal(
-    pbeta_post(2, likelihood = "nbinom", result = 2, sample_size = 10, failure_rate = 0.5),
-    pbeta(2, 0.5 + 10 * 0.5, 2 + 0.5))
-  expect_equal(pbeta_post(2, likelihood = "geometric", result = 2, sample_size = 10),
-               pbeta(2, 0.5 + 10, 2 + 0.5))
-  quants <- c(2, 3, 4, 5, 2, 3)
+    pbeta_post(1/2, likelihood = "nbinom", result = 2, sample_size = 10, failure_rate = 0.5),
+    pbeta(1/2, 0.5 + 10 * 0.5, 2 + 0.5))
+  expect_equal(pbeta_post(1/2, likelihood = "geometric", result = 2, sample_size = 10),
+               pbeta(1/2, 0.5 + 10, 2 + 0.5))
+  quants <- 1/c(2, 3, 4, 5, 2, 3)
   expect_length(pbeta_post(quants, "binom", result = 3, sample_size = 10), length(quants))
   expect_equal(pbeta_post(quants, "binom", result = 3, sample_size = 10),
                c(pbeta_post(quants[1], "binom", result = 3, sample_size = 10),
