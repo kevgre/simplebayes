2ed8907cc3197ae7801c20179ba84b5e4a14598c
Kevin Green <greenk12@tamu.edu>
2023-11-27 19:38:28 -0700

    Updated argument to avoid argument conflict

--- R/beta_priors.R
+++ R/beta_priors.R
@@ -1,23 +1,23 @@
 compute_beta_posterior_vals <- function(
-    a_prior, b_prior, likelihood, ...,
+    a_prior, b_prior, l_hood, ...,
     result = NULL, s_size = NULL, f_rate = NULL) {
-  if (likelihood == "binom") {
+  if (l_hood == "binom") {
     alpha_post <- result + a_prior
     beta_post <- s_size - result + b_prior
   }
-  if (likelihood == "nbinom") {
+  if (l_hood == "nbinom") {
     alpha_post <- a_prior + f_rate * s_size
     beta_post <- b_prior + result
   }
-  if (likelihood == "geometric") {
+  if (l_hood == "geometric") {
     alpha_post <- a_prior + s_size
     beta_post <- b_prior + result
   }
   c(alpha_post, beta_post)
 }
 
-# dbeta_post <- function(quantile, likelihood, ..., alpha_prior = 0.5, beta_prior = 0.5) {
-#   rlang::arg_match(likelihood, c("binom", "nbinom", "geometric"))
-#
-#   dbeta(quantile, alpha_post, beta_post, ...)
-# }
+dbeta_post <- function(quantile, likelihood, ..., alpha_prior = 0.5, beta_prior = 0.5) {
+  rlang::arg_match(likelihood, c("binom", "nbinom", "geometric"))
+
+  dbeta(quantile, alpha_post, beta_post, ...)
+}
