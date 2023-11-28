ba371273dfffcef8a7d69ccee35ebecdfe3a72fe
Kevin Green <greenk12@tamu.edu>
2023-11-27 21:01:24 -0700

    Removed superceded function

--- R/one_parameter.R
+++ R/one_parameter.R
@@ -1,9 +1,3 @@
-# TODO: add Jefferies priors
-dgamma_post <- function(quantile, x, sample_size, ..., alpha_prior, beta_prior) {
-  x <- sum(x)
-  stats::dgamma(quantile, x + alpha_prior, rate = sample_size + beta_prior, ...)
-}
-
 pgamma_post <- function(quantile, x, sample_size, ..., alpha_prior, beta_prior) {
   x <- sum(x)
   stats::pgamma(quantile, x + alpha_prior, sample_size + beta_prior, ...)
