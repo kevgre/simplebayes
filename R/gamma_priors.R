092edef34b23ccee252042d49c071db71a3b6efb
Kevin Green <greenk12@tamu.edu>
2023-11-27 21:01:08 -0700

    Wrote documentation

--- R/gamma_priors.R
+++ R/gamma_priors.R
@@ -1,3 +1,19 @@
+#' Update Gamma Parameters
+#'
+#' `update_gamma_parameters()` will update the parameters for a gamma prior for
+#' a given likelihood. Currently, these are the Poisson, exponential, gamma, and
+#' inverse gamma likelihoods.
+#'
+#' @param a_prior The shape parameter
+#' @param b_prior The rate parameter
+#' @param l_hood The likelihood of the model
+#' @param ... Placeholder for future capabilities. Currently ignored
+#' @param obs The observations from the experiment
+#' @param s_size The sample size from the experiment
+#' @param a_obs The observed shape parameter
+#'
+#' @returns A vector of length two where the shape parameter is first and then
+#' the rate parameter
 update_gamma_parameters <- function(
     a_prior, b_prior, l_hood, ..., obs = NULL, s_size = NULL, a_obs = NULL
     ) {
@@ -16,6 +32,24 @@ update_gamma_parameters <- function(
   c(alpha_post, beta_post)
 }
 
+#' Posterior Gamma PDF
+#'
+#' `dgamma_post()` computes the posterior PDF for a model with a gamma prior.
+#'
+#' @param quantile A vector of quantiles
+#' @param observations The observations from the model
+#' @param sample_size The sample size from the model
+#' @param likelihood The likelihood used
+#' @param ... Pass arguments to dgamma
+#' @param alpha_observed The observed shape parameter
+#' @param alpha_prior The prior shape parameter
+#' @param beta_prior The prior rate parameter
+#'
+#' @returns A vector of PDF results that is the same length as quantile.
+#' @export
+#'
+#' @examples
+# TODO: add Jefferies priors
 dgamma_post <- function(
     quantile, observations, sample_size, likelihood, ...,
     alpha_observed = NULL, alpha_prior = 1, beta_prior = 1) {
