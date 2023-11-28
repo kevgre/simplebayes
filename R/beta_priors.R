71a337d34fa25ed3c49eba8440f6d38940a2d076
Kevin Green <greenk12@tamu.edu>
2023-11-27 19:55:31 -0700

    Wrote documentation for functions and finished dbeta_post

--- R/beta_priors.R
+++ R/beta_priors.R
@@ -1,3 +1,20 @@
+#' Compute Beta Posterior Parameters
+#'
+#' `compute_beta_posterior_vals()` will compute the posterior parameter values
+#' for a model with a beta prior.
+#'
+#' @param a_prior The alpha prior value
+#' @param b_prior The beta prior value
+#' @param l_hood The likelihood for the model
+#' @param ... Placeholder for future capabilities. Currently ignored
+#' @param result The result of the experiment (either successes or failures)
+#' @param s_size The sample size of the experiment
+#' @param f_rate The failure rate for the model. Only used for the negative
+#' binomial likelihood.
+#'
+#' @returns A vector of length two where the first value is the alpha parameter
+#' and the second value is the beta parameter.
+#'
 compute_beta_posterior_vals <- function(
     a_prior, b_prior, l_hood, ...,
     result = NULL, s_size = NULL, f_rate = NULL) {
@@ -16,8 +33,25 @@ compute_beta_posterior_vals <- function(
   c(alpha_post, beta_post)
 }
 
+#' Sample Posterior Beta PDF
+#'
+#' `dbeta_post()` will compute the posterior distribution of a model with a beta
+#' prior. Currently, the binomial, negative binomial, and geometric likelihoods
+#' can be used.
+#'
+#' @param quantile A vector of quantiles
+#' @param likelihood The likelihood to be used
+#' @param ... Pass further arguments such as the sample size, experiment result,
+#' or failure rate.
+#' @param alpha_prior The prior value for the alpha parameter
+#' @param beta_prior The prior value for the beta parameter
+#'
+#' @returns A vector of the same length as quantile.
+#' @export
+#'
+#' @examples
 dbeta_post <- function(quantile, likelihood, ..., alpha_prior = 0.5, beta_prior = 0.5) {
   rlang::arg_match(likelihood, c("binom", "nbinom", "geometric"))
-
-  dbeta(quantile, alpha_post, beta_post, ...)
+  post_vals <- compute_beta_posterior_vals(alpha_prior, beta_prior, likelihood, ...)
+  dbeta(quantile, post_vals[1], post_vals[2])
 }
