3c64d850d9f42edf49620b631b568b75aa0177ee
Kevin Green <greenk12@tamu.edu>
2023-11-27 20:00:32 -0700

    Wrote tests for pbeta_post

--- tests/testthat/test-beta_priors.R
+++ tests/testthat/test-beta_priors.R
@@ -30,3 +30,22 @@ test_that("dbeta_post works", {
                  dbeta_post(quants[5], "binom", result = 3, s_size = 10),
                  dbeta_post(quants[6], "binom", result = 3, s_size = 10)))
 })
+
+test_that("pbeta_post works", {
+  expect_error(pbeta_post(2, "poisson"))
+  expect_equal(pbeta_post(2, likelihood = "binom", result = 2, s_size = 10),
+               pbeta(2, 0.5 + 2, 10 - 2 + 0.5))
+  expect_equal(pbeta_post(2, likelihood = "nbinom", result = 2, s_size = 10, f_rate = 0.5),
+               pbeta(2, 0.5 + 10 * 0.5, 2 + 0.5))
+  expect_equal(pbeta_post(2, likelihood = "geometric", result = 2, s_size = 10),
+               pbeta(2, 0.5 + 10, 2 + 0.5))
+  quants <- c(2, 3, 4, 5, 2, 3)
+  expect_length(pbeta_post(quants, "binom", result = 3, s_size = 10), length(quants))
+  expect_equal(pbeta_post(quants, "binom", result = 3, s_size = 10),
+               c(pbeta_post(quants[1], "binom", result = 3, s_size = 10),
+                 pbeta_post(quants[2], "binom", result = 3, s_size = 10),
+                 pbeta_post(quants[3], "binom", result = 3, s_size = 10),
+                 pbeta_post(quants[4], "binom", result = 3, s_size = 10),
+                 pbeta_post(quants[5], "binom", result = 3, s_size = 10),
+                 pbeta_post(quants[6], "binom", result = 3, s_size = 10)))
+})
