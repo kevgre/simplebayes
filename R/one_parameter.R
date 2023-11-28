e20b28c795cf4b6d2d2d3b0dbe0f129234e2cb49
Kevin Green <greenk12@tamu.edu>
2023-11-27 20:41:36 -0700

    Removed superceded functions

--- R/one_parameter.R
+++ R/one_parameter.R
@@ -1,33 +1,3 @@
-#' Sample from beta CDF
-#'
-#' Samples from the Cumulative Density Function for the beta distribution.
-#'
-#' @param quantile Vector of quantiles
-#' @param successes The number of successes in the binomial experiment
-#' @param sample_size The sample size for the binomial experiment
-#' @param ... Pass additional arguments to the pbeta function
-#' @param alpha_prior The prior value for alpha
-#' @param beta_prior The prior value for beta
-#'
-#' @returns A vector of probabilities the same length as the quantiles
-#' @export
-#'
-#' @examples
-pbeta_post <- function(
-    quantile, successes, sample_size, ..., alpha_prior = 1/2, beta_prior = 1/2
-    ) {
-  failures <- sample_size - successes
-  stats::pbeta(quantile, successes + alpha_prior, failures + beta_prior, ...)
-}
-
-# TODO: update documentation to appear in a single file
-rbeta_post <- function(
-    n, successes, sample_size, ..., alpha_prior = 1/2, beta_prior = 1/2
-) {
-  failures <- sample_size - successes
-  stats::rbeta(n = n, successes + alpha_prior, failures + beta_prior, )
-}
-
 # TODO: add Jefferies priors
 dgamma_post <- function(quantile, x, sample_size, ..., alpha_prior, beta_prior) {
   x <- sum(x)
