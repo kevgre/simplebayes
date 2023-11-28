c4215eb2edc372467b68ed27cf0b6694b6317d58
Kevin Green <greenk12@tamu.edu>
2023-11-27 19:57:16 -0700

    Removed rewritten function

--- R/one_parameter.R
+++ R/one_parameter.R
@@ -1,26 +1,3 @@
-#' Sample Posterior Beta PDF
-#'
-#' `dbeta_post()` will sample the posterior beta probability distribution function
-#' in a beta-binomial model.
-#'
-#' @param quantile vector of quantiles
-#' @param successes number of success in the binomial experiment
-#' @param sample_size number of trials in the binomial experiment
-#' @param ... Placeholder for future capabilities. Currently ignored.
-#' @param alpha_prior The prior for the alpha parameter
-#' @param beta_prior The prior for the beta parameter
-#'
-#' @returns A vector of probabilities the same length as quantile
-#' @export
-#'
-#' @examples
-dbeta_post <- function(
-    quantile, successes, sample_size, ..., alpha_prior = 1/2, beta_prior = 1/2
-    ) {
-  failures <- sample_size - successes
-  stats::dbeta(quantile, successes + alpha_prior, failures + beta_prior, ...)
-}
-
 #' Sample from beta CDF
 #'
 #' Samples from the Cumulative Density Function for the beta distribution.
