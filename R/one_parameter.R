cecb14f95b4c00c980d785872326b8eaf5cb2f3a
Kevin Green <greenk12@tamu.edu>
2023-11-27 21:05:48 -0700

    Removed superceded function

--- R/one_parameter.R
+++ R/one_parameter.R
@@ -1,8 +1,3 @@
-# pgamma_post <- function(quantile, x, sample_size, ..., alpha_prior, beta_prior) {
-#   x <- sum(x)
-#   stats::pgamma(quantile, x + alpha_prior, sample_size + beta_prior, ...)
-# }
-
 rgamma_post <- function(n, x, sample_size, ..., alpha_prior, beta_prior) {
   x <- sum(x)
   stats::rgamma(n = n, shape = x + alpha_prior, rate = sample_size + beta_prior)
