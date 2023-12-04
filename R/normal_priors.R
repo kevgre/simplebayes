7495cf266387782b200fb90997767df88d510e3c
Kevin Green <greenk12@tamu.edu>
2023-12-03 19:18:46 -0700

    Updated arguments for update_normal_parameters to avoid clash with wrapper functions

--- R/normal_priors.R
+++ R/normal_priors.R
@@ -1,21 +1,29 @@
 #' Update Normal Parameters
 #'
-#' @param mean_prior The prior value for the mean
+#' @param mu_prior The prior value for the mean
 #' @param var_prior The prior value for the variance
 #' @param obs The observations from the model
 #' @param ... Placeholder for future capabilities. Currently ignored
-#' @param precision_prior The prior value for the precision
+#' @param prec_prior The prior value for the precision
 #'
 #' @returns A vector of length 2 where the posterior mean is first followed by
 #' the posterior variance
 update_normal_parameters <- function(
-    mean_prior, var_prior, obs, ..., precision_prior = 1/var_prior
+    mu_prior, var_prior, obs, ..., prec_prior = 1/var_prior
     ) {
-  precision <- 1/var(obs)
-  nprecision <- length(obs) * precision
-  mean_numerator <- precision_prior * mean_prior + precision * sum(obs)
-  precision_post <- (precision_prior + nprecision)
-  mean_post <- mean_numerator/precision_post
+  prec <- 1/var(obs)
+  nprec <- length(obs) * prec
+  mu_numerator <- prec_prior * mu_prior + prec * sum(obs)
+  prec_post <- (prec_prior + nprec)
+  mu_post <- mu_numerator/prec_post
 
-  c(mean_post, 1/precision_post)
+  c(mu_post, 1/prec_post)
+}
+
+
+dnorm_post <- function(
+    quantiles, observations, ...,
+    mean_prior = 1, variance_prior = 1 / sd(observations),
+    precision_prior = 1/variance_prior) {
+  norm_post <- update_normal_parameters()
 }
