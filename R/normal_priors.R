430aa6a91ce3a6458dbdab5f25bb4b45e70401db
Kevin Green <greenk12@tamu.edu>
2023-12-03 19:36:21 -0700

    Wrote function for posterior pdf

--- R/normal_priors.R
+++ R/normal_priors.R
@@ -21,9 +21,36 @@ update_normal_parameters <- function(
 }
 
 
+#' Posterior Normal Distribution
+#'
+#' `dnorm_post()` computes values from the posterior PDF for a model with a
+#' normal prior.
+#'
+#' `pnorm_post()` computes values from the posterior CDF for a model with a
+#' normal prior
+#'
+#' `rnorm_post` generates a random value from the posterior distribution for a
+#' model with a normal prior.
+#'
+#' These posterior functions assume that the variance for the model is fixed.
+#'
+#' @param quantiles A vector of quantiles
+#' @param n The number of random values to generate
+#' @param observations The observations from the model
+#' @param ... Pass parameters to the underlying functions such as log or
+#' lower.tail
+#' @param mean_prior The prior value for the mean
+#' @param variance_prior The prior value for the variance
+#' @param precision_prior The prior value for the variance
+#'
+#' @returns A vector the same length as `quantiles` or `n`
+#' @export
+#'
+#' @examples
 dnorm_post <- function(
     quantiles, observations, ...,
     mean_prior = 1, variance_prior = 1 / sd(observations),
     precision_prior = 1/variance_prior) {
-  norm_post <- update_normal_parameters()
+  norm_post <- update_normal_parameters(mean_prior, variance_prior, observations)
+  dnorm(quantiles, norm_post[1], norm_post[2], ...)
 }
