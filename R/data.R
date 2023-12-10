6310127106bc007d7dc0c2d333f8415637228f75
Kevin Green <greenk12@tamu.edu>
2023-12-09 18:49:23 -0700

    Wrote documentation for life expectancy data set

---
+++ R/data.R
@@ -0,0 +1,41 @@
+#' World Health Organization Life Expectancy Data
+#'
+#' The 2012 edition of the life expectancy data from the World Health
+#' Organization (WHO).
+#'
+#' @format ## `life_expectancy`
+#' A data frame with 129 rows and 22 columns:
+#' \describe{
+#'    \item{Country}{Country name}
+#'    \item{Year}{Data year}
+#'    \item{Status}{Developed or developing status}
+#'    \item{Life.expectancy}{Life Expectancy in Age}
+#'    \item{Adult.Mortality}{Adult Mortality Rates (probability of dying between
+#'    15 and 60 years per 1000 population)}
+#'    \item{infant.deaths}{Number of Infant Deaths per 1000 population}
+#'    \item{Alcohol}{Per capita consumption of alcohol (15+ age, liters of pure
+#'    alcohol)}
+#'    \item{percentage.expenditure}{Expenditure on health as a percentage of
+#'    Gross Domestic Product per capita (%)}
+#'    \item{Hepatitis.B}{Hepatitix B immunization coverage for 1 year olds (%)}
+#'    \item{Measles}{Number of reported measles cases per 1000 population}
+#'    \item{BMI}{Average Body Mass Index for population}
+#'    \item{under.five.deaths}{Number of under age five deaths per 1000 population}
+#'    \item{Polio}{Polio immunization coverage for 1 year olds (%)}
+#'    \item{Total.expenditure}{Government expenditure on health as a percentage
+#'    of total government expenditure (%)}
+#'    \item{Diphtheria}{Diphtheria, tetanus toxoid, and pertussis immunization
+#'    coverage among 1 year old (%)}
+#'    \item{HIV.AIDS}{Deaths per 1000 live births due to HIV or AIDS (0-4 years)}
+#'    \item{GDP}{Gross Domestic Product per capita (USD)}
+#'    \item{Population}{Country's population}
+#'    \item{thinness..1.19.years}{Prevalence of thinness among children and
+#'    adolescents between ages 10 and 19 (%)}
+#'    \item{thinness.5.9.years}{Prevalence of thinness among childreng between
+#'    ages 5 and 9 (%)}
+#'    \item{Incom.composition.of.resoucres}{Human Development index in terms of
+#'    income composition of resources (between 0 and 1)}
+#'    \item{Schooling}{Years of schooling}
+#' }
+#' @source <https://www.kaggle.com/datasets/kumarajarshi/life-expectancy-who/>
+"life_expectancy"
